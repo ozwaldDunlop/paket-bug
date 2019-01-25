d594e72d1749fb3463204e4854afe6d1e197a8e2
Oliver Dunthorne <oliver.dunthorne@myunidays.com>
2019-01-25 13:33:58 +0000

    added nuget version of example

---
+++ src/ConsoleApp1Core/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary1;
+using Ionic.Zlib;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GZipStream stream = new Class1().Stream;
+        }
+    }
+}
