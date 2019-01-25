7295c94e0b00ea0eb81e9268147e82382148d54d
Andrew Bullock <trullock@gmail.com>
2019-01-25 12:46:56 +0000

    added example repro

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Ionic;
+using Ionic.Zlib;
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		public GZipStream Stream;
+
+		public Class1()
+		{
+			this.Stream = new GZipStream(new MemoryStream(), CompressionMode.Compress);
+		}
+	}
+}
